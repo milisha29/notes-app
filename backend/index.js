34ff9364aa0514f79165f3326ff45e51d1cb2d3e
milisha29 <guptamilisha@gmail.com>
2022-06-23 10:55:08 +0530

    added gitignore

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ connectDB();
 app.use(express.json());
 app.use('/notes',myroutes);
 
-
+const port = process.env.PORT || 5000;
 app.listen(5000,()=>{
-    console.log(`Server is running at port ${5000}`);
+    console.log(`Server is running at port ${port}`);
 })
